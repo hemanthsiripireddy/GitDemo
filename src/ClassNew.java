b34cd7efaff16a7890f854117c6f3a8dbee334e7
hemanthsiripireddy <siripihemanth19@gmail.com>
2022-04-07 23:56:39 +0530

    Architect Asian development

---
+++ src/ClassNew.java
@@ -0,0 +1,11 @@
+
+public class ClassNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is new class");
+		System.out.println("we are in develop branch");
+
+	}
+
+}
